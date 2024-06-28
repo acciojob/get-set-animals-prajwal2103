1c9336855c18c3267b6be98bfe7e2aa5cbdd119d
prajwal2103 <137304916+prajwal2103@users.noreply.github.com>
2024-06-28 14:37:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,14 +2,15 @@
 class Animal {
 	constructor(species){
 		this._species=species;
+	}
 		get species(){
 			return this._species;
 		}
 		makeSound(){
-          console.log("The Golden Retriever makes a sound)
+          console.log(`The ${this._species} makes a sound`)
 		}
 	}
-}
+
 
 class Dog extends Animal {
 	constructor(species){
